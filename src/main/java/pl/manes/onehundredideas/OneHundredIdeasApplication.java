748a419aa630d9b8242c9a5e48ef58b278f5f12d
Manes79 <mszornel@gmail.com>
2022-07-07 16:44:36 +0200

    initial commit

---
+++ src/main/java/pl/manes/onehundredideas/OneHundredIdeasApplication.java
@@ -0,0 +1,13 @@
+package pl.manes.onehundredideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneHundredIdeasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneHundredIdeasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/manes/onehundredideas/OneHundredIdeasApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.manes.onehundredideas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneHundredIdeasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
